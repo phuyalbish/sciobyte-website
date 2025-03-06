857bdf81b43934fce957abb0c45ec57737170135
phuyalbish <phuyalbish@gmail.com>
2025-03-06 21:00:24 +0545

    Parallex Complete and Started Youtube SEction

--- src/components/pages/home/Parallex.jsx
+++ src/components/pages/home/Parallex.jsx
@@ -8,12 +8,17 @@ import img7 from "@/assets/image7.png";
 import img8 from "@/assets/image8.png";
 import img9 from "@/assets/image9.png";
 
+import catimg1 from "@/assets/hikingCategory.png";
+import catimg2 from "@/assets/toursCategory.png";
+import catimg3 from "@/assets/trekkingCategory.png";
+import CategoryTile from "../../tiles/CategoryTile";
+
 export default function Parallax() {
   const { scrollY } = useScroll();
 
   // Define max scroll limit (stop movement after this)
   const maxScroll = 1000;
-  const scrollThreshold = 500;
+  const scrollThreshold = 300;
 
   const scrollYAdjusted = useTransform(
     scrollY,
@@ -24,10 +29,10 @@ export default function Parallax() {
   // Define vertical transformations
   const y1 = useTransform(scrollYAdjusted, [0, maxScroll], [0, 0]);
   const y2 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -150]);
-  const y3 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -100]);
-  const y5 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -150]);
+  const y3 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -50]);
+  const y5 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -100]);
   const y6 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -20]);
-  const y7 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -250]);
+  const y7 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -150]);
   const y8 = useTransform(scrollYAdjusted, [0, maxScroll], [0, 50]);
   const y9 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -50]);
 
@@ -41,8 +46,14 @@ export default function Parallax() {
   const x8 = useTransform(scrollYAdjusted, [0, maxScroll], [0, -20]);
   const x9 = useTransform(scrollYAdjusted, [0, maxScroll], [0, 20]);
 
+  const categories = [
+    { img: catimg1, name: "Trek" },
+    { img: catimg2, name: "Adventure" },
+    { img: catimg3, name: "Camping" },
+  ];
+
   return (
-    <div className="relative m-0 p-0 w-screen h-screen overflow-hidden  borer mt-20">
+    <div className="relative m-0 p-0 w-screen h-[150vh] overflow-hidden">
       <div className="w-screen overflow-hidden">
         <motion.img
           src={img1}
@@ -76,7 +87,7 @@ export default function Parallax() {
           src={img5}
           alt=""
           className="absolute z-20 min-w-[102vw]"
-          style={{ y: y5, x: x5, top: `180px`, right: `0px` }}
+          style={{ y: y5, x: x5, top: `250px`, right: `0px` }}
         />
       </div>
 
@@ -86,7 +97,7 @@ export default function Parallax() {
           src={img6}
           alt=""
           className="absolute z-10 min-w-[102vw]"
-          style={{ y: y6, x: x6, top: `270px`, left: `0px` }}
+          style={{ y: y6, x: x6, top: `300px`, left: `0px` }}
         />
       </div>
 
@@ -115,6 +126,18 @@ export default function Parallax() {
           className="absolute -z-20 min-w-[102vw]"
           style={{ y: y9, x: x9, top: `150px`, right: `0px` }}
         />
+      </div>
+      <div className="absolute w-screen top-[90vh] z-50 flex flex-col gap-10 flex-grow-0 justify-center items-center">
+        <div className="bg-white py-2 px-5 text-blue-500   w-fit font-base text-2xl  rounded-lg">
+          Your <span className="font-bold">PREFERENCE</span> is our{" "}
+          <span className="font-bold">PRIORITY</span>
+        </div>
+
+        <div className="flex gap-5 justify-center">
+          {categories.map((category, index) => (
+            <CategoryTile key={index} img={category.img} name={category.name} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/pages/home/YTSection.jsx
+++ src/components/pages/home/YTSection.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import ytbg from "@/assets/YTBG.png";
+import { FaYoutube } from "react-icons/fa";
+
 function YTSection() {
   return (
-    <div className="relative">
-      <img
-        src={ytbg}
-        alt=""
-        className="w-screen h-screen object-cover absolute z-0"
-      />
+    <div className="relative  w-screen h-[80vh]">
+      <img src={ytbg} alt="" className="w-screen   object-cover absolute z-0" />
 
-      <div className="w-screen h-screen z-10 relative flex ">
-        <div className="textConten w-2/5 flex flex-col gap-5 px-20 justify-center">
-          <div className="text-4xl text-left text-white font-bold">
-            Let`s <span className="text-red-500 font-bold ">Youtube</span>
+      <div className=" z-10 bg-cover relative w-full h-full flex items-center">
+        <div className="textConten w-7/12  flex flex-col gap-10 pl-20 justify-center">
+          <div className="text-5xl text-left text-white font-bold flex flex-col ">
+            Let`s
           </div>
-          <div className="text-6xl text-white font-bold text-left leading-normal">
-            Tales from the Trekking Trials
-          </div>
-          <div className="text-3xl text-white font-bold text-left">
-            Be ready to find out new tale.
-          </div>
+          <span className="text-red-500 font-base text-9xl flex gap-5 items-center ">
+            <FaYoutube />
+            Youtube
+          </span>
         </div>
       </div>
     </div>
