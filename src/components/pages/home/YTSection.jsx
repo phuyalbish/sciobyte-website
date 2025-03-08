fb021e6377ff664796ba0f255305f340b8968614
phuyalbish <phuyalbish@gmail.com>
2025-03-08 10:04:12 +0545

    YoutubeSectionComplete

--- src/components/pages/home/YTSection.jsx
+++ src/components/pages/home/YTSection.jsx
@@ -1,22 +1,39 @@
 import React from "react";
-import ytbg from "@/assets/YTBG.png";
+import ytbg from "@/assets/YTBG.jpg";
 import { FaYoutube } from "react-icons/fa";
-
+import YTCarousel from "./YTCarousel";
+import img7 from "@/assets/carousel1.png";
+import img8 from "@/assets/carousel2.png";
+const slides = [img7, img8];
 function YTSection() {
   return (
-    <div className="relative  w-screen h-[80vh]">
-      <img src={ytbg} alt="" className="w-screen object-contain absolute z-0" />
+    <div className="relative  w-screen h-[80vh] ">
+      <img
+        src={ytbg}
+        alt=""
+        className="w-screen h-full object-cover absolute z-0"
+      />
 
-      <div className=" z-10 bg-cover relative w-full h-full flex items-center">
-        <div className="textConten w-7/12  flex flex-col gap-10 pl-20 justify-center">
+      <div className="relative z-10 bg-cover w-full h-full flex items-center ">
+        <div className="textConten w-7/12 h-full flex flex-col gap-10 pl-20 justify-center ">
           <div className="text-5xl text-left text-white font-bold flex flex-col ">
             Let`s
           </div>
-          <span className="text-red-500 font-base lg:text-9xl flex gap-5 items-center ">
+          <span className="text-red-500 font-base lg:text-7xl flex gap-5 items-center ">
             <FaYoutube />
             Youtube
           </span>
         </div>
+        <YTCarousel>
+          {slides.map((s, index) => (
+            <img
+              key={index}
+              src={s}
+              alt={`Slide ${index}`}
+              className="w-[28vw]"
+            />
+          ))}
+        </YTCarousel>
       </div>
     </div>
   );
