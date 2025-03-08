9b62414552c1ceabe224d7aff4f781320d80f664
seejn <seejnmaharjan@gmail.com>
2025-03-08 16:30:49 +0545

    created component: TrekSection

---
+++ src/components/pages/home/TrekSection.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import TrekTile from "@/components/tiles/TrekTile.jsx";
+import { fetchTreks } from "@/apis/treks.js";
+import { v4 as uuidv4 } from "uuid";
+
+const TrekSection = ()  => {
+    const [treks, setTreks] = useState([]);
+    
+    useEffect(() => {
+        ( async () => {
+            const response = await fetchTreks();
+            const treksData = response?.data?.results;
+            setTreks(treksData);
+        })();
+    }, []);
+    
+
+    return(
+        <>
+            <section className="p-4">
+                <h1 className="text-[2.3rem] mb-[3rem] font-semibold">Your Tale Begins <span className="text-primary">NOW!</span></h1>
+                <div className="flex flex-col md:flex-row gap-[1.5rem] justify-center items-center">
+                    {
+                        treks?.map(trek => <TrekTile key={uuidv4()} data={trek} />)
+                    }
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default TrekSection;
---
+++ src/components/tiles/TrekTile.jsx
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+
+const JourneyDetailsPanel = ({ detail }) => {
+    console.log(detail);
+    return (
+        <>
+            <p className="flex gap-[1rem]">
+                <img src={detail?.icon} alt="TrekType.svg" />
+                <span className="text-center">
+                    <span className="block text-sm">{detail?.key}</span>
+                    <strong className="block text-base">{detail?.value}</strong>
+                </span>
+            </p>
+        </>
+    )
+}
+
+const TrekTile = ({ data }) => {
+    const { name, price, image, difficulty, formatted_duration } = data;
+    const journeyDetails = [
+        { icon: '/TrekType.svg', key: "Type", value: "Trek" },
+        { icon: '/duration.svg', key: "Duration", value: formatted_duration },
+        { icon: '/difficult.svg', key: "Difficulty Level", value: difficulty?.name },
+    ]
+
+    return (
+        <>
+            <div className="max-w-sm flex flex-col gap-[0.5rem] rounded overflow-hidden shadow-md bg-white p-5">
+                <div className="relative">
+                    <img className="rounded w-full h-64 object-cover object-center" src={image} alt="Annapurna Base Camp Circuit" />
+                    <div className="flex items-center mb-2">
+                        <span className="absolute top-[0.5rem] left-[0.5rem] bg-secondary text-white text-base  rounded-xl font-bold px-[1rem] py-[0.5rem]">Best Price</span>
+                        <div className="absolute top-[0.5rem] right-[0.5rem] h-[2.3rem] w-[2.3rem] bg-secondary rounded-xl p-[0.3rem]">
+                            <img className="h-full w-full" src="/group.svg" alt="Group Icon" />
+                        </div>
+                    </div>
+                </div>
+                <h2 className="font-bold text-xl mb-2 text-left">{name}</h2>
+                <div className="flex justify-between items-end mb-1 w-full">
+                    <span className="text-yellow-500 text-2xl">★★★★☆</span>
+                    <p className="text-gray-600 text-right">Price Starting From <br /> <span className="text-primary font-bold text-3xl">USD {price}</span></p>
+                </div>
+                <div className="flex justify-between text-gray-600 text-sm mt-2 gap-[2rem]">
+                    {
+                        journeyDetails.map((detail, index) => <JourneyDetailsPanel key={index} detail={detail} />)
+                    }
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default TrekTile;
