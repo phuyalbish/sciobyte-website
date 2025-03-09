fcd74496c43572b2037432ee21efaca14f794014
seejn <seejnmaharjan@gmail.com>
2025-03-09 18:36:26 +0545

    added method to fetch faqs

---
+++ src/apis/faqs.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { BASE_API_URL } from "@/config/baseurl.js";
+
+export const fetchFAQs = async () => {
+    try{
+        const response = await axios({
+            url: `${BASE_API_URL}/faqs/all/`,
+            method: "GET"
+        });
+        return response;
+    }catch(error){
+        console.log("Error while fetching trek data: ", error);
+    }
+}
