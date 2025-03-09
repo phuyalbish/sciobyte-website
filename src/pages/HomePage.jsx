9bb1c5eaa56db5c8c69d6cc8ada624b98d9a39ab
phuyalbish <phuyalbish@gmail.com>
2025-03-09 20:31:58 +0545

    FAQ added i¥in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import SplashSection from "@/components/pages/home/SplashSection";
 import TrekSection from "@/components/pages/home/TrekSection.jsx";
+import FAQSection from "@/components/pages/home/FAQSection.jsx";
 import Parallex from "@/components/pages/home/Parallex.jsx";
 import YTSection from "@/components/pages/home/YTSection.jsx";
 import whatsapp from "@/assets/whatsapp.png";
-import HomeStaySection from "@/components/pages/home/HomeStaySection.jsx"
+import HomeStaySection from "@/components/pages/home/HomeStaySection.jsx";
 import ReviewSection from "@/components/pages/home/ReviewSection";
 import InstagramSection from "@/components/pages/home/InstagramSection";
 
 function HomePage() {
   return (
     <div className="flex flex-col gap-[3rem]">
-        <SplashSection />
-        <TrekSection />
-        <Parallex />
-        <HomeStaySection />
+      <SplashSection />
+      <TrekSection />
+      <Parallex />
+      <HomeStaySection />
       <div className="flex flex-col">
         <YTSection />
         <ReviewSection />
       </div>
       <InstagramSection />
+      <FAQSection />
       <img
         src={whatsapp}
         alt=""
