9342de139ef22398725c1179beabdf8ca197b8c5
seejn <seejnmaharjan@gmail.com>
2025-03-13 22:25:28 +0545

    added route for blog_detail page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,6 +4,8 @@ import Home from "@/pages/HomePage.jsx";
 import AboutUs from "@/pages/AboutUsPage.jsx";
 import Contact from "@/pages/ContactPage.jsx";
 import Blogs from "@/pages/BlogListPage.jsx";
+import BlogDetail from "@/pages/BlogDetailPage.jsx";
+
 import NotFound from "@/pages/NotFoundPage.jsx";
 
 const AppRoutes = () => {
@@ -13,6 +15,7 @@ const AppRoutes = () => {
       <Route path="/about" element={<AboutUs />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/blogs" element={<Blogs />} />
+      <Route path="/blog/:slug" element={<BlogDetail />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
