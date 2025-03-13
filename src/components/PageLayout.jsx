14bb78b7eb65e50d01a3ca3dbcff050bb2095b32
seejn <seejnmaharjan@gmail.com>
2025-03-13 22:24:04 +0545

    moved whatsapp icon from homepage -> pageLayout

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -2,15 +2,22 @@ import Footer from "@/components/Footer.jsx";
 import Header from "@/components/Header.jsx";
 import Navbar from "@/components/Navbar";
 
+import whatsapp from "@/assets/whatsapp.png";
+
 export default function PageLayout({ children }) {
   return (
     <div className="flex flex-col ">
-      <div className="flex sticky top-0 w-screen flex-col z-50">
+      <div className="flex sticky top-0 w-full flex-col z-50">
         <Header />
         <Navbar />
       </div>
-      <main className="flex-1 bg-gray-100  overflow-auto">
+      <main className="flex-1 bg-gray-100">
         {children}
+        <img
+          src={whatsapp}
+          alt=""
+          className="w-16 h-16  object-cover z-40 fixed bottom-5 left-5 "
+        />
       </main>
       <Footer />
     </div>
