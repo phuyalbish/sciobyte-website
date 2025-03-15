e375fd23574d106b4868b0bd98f4d33cae16bb76
seejn <seejnmaharjan@gmail.com>
2025-03-15 16:44:49 +0545

    updated styles
    - added hover effect on tiles
    - hidden the price
    - updated gap between the tile card
    - layout -> grid

--- src/components/pages/home/SpontaneousTrekSection.jsx
+++ src/components/pages/home/SpontaneousTrekSection.jsx
@@ -18,23 +18,26 @@ function SpontaneousTrekSection() {
 
   return (
     <>
-      <div className="flex flex-col mt-10 gap-5 ">
-        <div className="md:text-xl text-lg lg:text-3xl  flex flex-wrap items-center justify-center gap-2 ">
-          <span className="font-bold text-white bg-G200 p-2 rounded-2xl mx-2">
-            SPONTANEOUS
-          </span>
-          Decision
-          <span className="font-bold text-white bg-G200 p-2 rounded-2xl mx-2">
-            ADVENTEROUS
-          </span>
-          Trials
+      <div className="px-[2rem] md:px-[4.5rem] flex flex-col gap-10">
+        <div className="flex flex-col mt-10 gap-5">
+          <div className="md:text-xl text-lg lg:text-3xl  flex flex-wrap items-center justify-center gap-[0.1rem] ">
+            <span className="font-bold text-white bg-G200 p-2 rounded-2xl mx-2">
+              SPONTANEOUS
+            </span>
+            Decision
+            <span className="font-bold text-white bg-G200 p-2 rounded-2xl mx-2">
+              ADVENTEROUS
+            </span>
+            Trials
+          </div>
+          <div className="text-lg">Last Moment Deals</div>
         </div>
-        <div className="text-lg">Last Moment Deals</div>
-      </div>
-      <div className="flex relative flex-wrap gap-1 w-[90vw]  sm:ml-[5vh] place-items-center justify-center items-center">
-        {treks?.map((trek) => (
-          <SopontaneousTrekTile key={uuidv4()} data={trek} />
-        ))}
+        {/* <div className="flex relative flex-wrap gap-[2rem] w-full px-[1rem] md:px-0 sm:ml-[5vh] place-items-center justify-center items-center"> */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 relative gap-[2rem]">
+          {treks?.map((trek) => (
+            <SopontaneousTrekTile key={uuidv4()} data={trek} />
+          ))}
+        </div>
       </div>
     </>
   );
