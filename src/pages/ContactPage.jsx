fcfbcea7ffabf7725906c9537144deb51ff80a7c
seejn <seejnmaharjan@gmail.com>
2025-03-15 10:23:04 +0545

    initiated page: contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <>
+      <section>
+        <div className="py-[2rem]">
+          <h1 className="text-4xl font-semibold">Contact Us</h1>
+          <p>Empowering individuals! to create their stories.</p>
+        </div>
+      </section>;
+    </>
+  );
 }
 
 export default ContactPage;
