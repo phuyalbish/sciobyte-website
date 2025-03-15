3f9b2e0342df82d2072893d1a0c3bae427cefc34
seejn <seejnmaharjan@gmail.com>
2025-03-15 16:35:57 +0545

    removed heading: "Latest Travel Blog"

--- src/components/pages/blogs/LatestTravelBlog.jsx
+++ src/components/pages/blogs/LatestTravelBlog.jsx
@@ -6,7 +6,7 @@ const LatestTravelBlog = ({latestBlogs}) => {
     return (
         <>
             <div className="container">
-                <h1 className="text-[2.375rem] font-semibold py-[2rem]">Latest Travel Blog</h1>
+                {/* <h1 className="text-[2.375rem] font-semibold py-[2rem]">Latest Travel Blog</h1> */}
                 <div className="mx-auto px-[2rem] md:px-[4.5rem] mb-[4rem]">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         {
