10670ff5b840b67c48323a8a01f7da69a7ccefb1
phuyalbish <phuyalbish@gmail.com>
2025-03-16 21:22:10 +0545

    About Us deleted

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -12,7 +12,19 @@ function CompanyPage() {
           <Hero />
           <div className="flex flex-col gap-[0.8rem] pt-[5rem]">
             <h3 className="text-lg font-semibold">Hello Trekkers Pvt. Ltd.</h3>
-            <p>Hello Trekkers Pvt. Ltd. was founded in the year 2025 with the primary goal of delivering exceptional trekking experiences to adventure enthusiasts from Nepal and across the globe. Our company was established with a strong commitment to promoting Nepal's natural beauty and cultural heritage while ensuring that every trekker—whether local or international—receives the highest standard of service, safety, and hospitality. We strive to create memorable journeys by combining professional guidance, personalized services, and authentic experiences that allow travelers to explore the majestic landscapes of Nepal in a meaningful and enjoyable way.</p>
+            <p>
+              Hello Trekkers Pvt. Ltd. was founded in the year 2025 with the
+              primary goal of delivering exceptional trekking experiences to
+              adventure enthusiasts from Nepal and across the globe. Our company
+              was established with a strong commitment to promoting Nepal's
+              natural beauty and cultural heritage while ensuring that every
+              trekker—whether local or international—receives the highest
+              standard of service, safety, and hospitality. We strive to create
+              memorable journeys by combining professional guidance,
+              personalized services, and authentic experiences that allow
+              travelers to explore the majestic landscapes of Nepal in a
+              meaningful and enjoyable way.
+            </p>
           </div>
           <Team />
           <HomeStay />
