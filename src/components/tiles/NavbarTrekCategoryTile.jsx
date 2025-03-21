092ef4a8c04fe9cfea1600d58ebf458320d22fed
seejn <seejnmaharjan@gmail.com>
2025-03-21 14:05:28 +0545

    removed shadow for dropdown menus

--- src/components/tiles/NavbarTrekCategoryTile.jsx
+++ src/components/tiles/NavbarTrekCategoryTile.jsx
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 export const BASE_MEDIA_URL = import.meta.env.VITE_BASE_MEDIA_URL;
 
-function NavbarTrekCategoryTile({ image, type, name, id, setDropdowns, onClick }) {
+function NavbarTrekCategoryTile({ image, type, name, id, setDropdowns }) {
   const navigate = useNavigate();
   return (
     <div
-      className="cursor-pointer flex gap-2 p-1 items-center shadow-md rounded-md hover:bg-white/25   hover:shadow-sm"
+      className="cursor-pointer flex gap-2 p-1 items-center rounded-md hover:bg-white/25   hover:shadow-sm"
       onClick={() => {
         setDropdowns({})
         navigate(`/${type}/${id}`)
