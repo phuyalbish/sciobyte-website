0b0613736ec983cfc84323a499993e90a3ebf826
phuyalbish <phuyalbish@gmail.com>
2025-03-22 11:46:37 +0545

    extra heading added

--- src/components/pages/indivisualtrek/TrekOverviewSection.jsx
+++ src/components/pages/indivisualtrek/TrekOverviewSection.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-function TrekOverviewSection({ data }) {
-  return <section id="overview">{data}</section>;
+export const BASE_MEDIA_URL = import.meta.env.VITE_BASE_MEDIA_URL;
+function TrekOverviewSection({ description, challenges }) {
+  return (
+    <section id="overview" className="flex flex-col gap-5">
+      {description}
+      <div className="text-base font-semibold">Challenges:</div>
+      <div className="flex flex-wrap gap-2">
+        {challenges?.length ? (
+          challenges.map((item, index) => (
+            <div
+              key={index}
+              className={` py-2 px-4 rounded-xl flex gap-3 items-center border hover:border-B75 bg-transparent cursor-pointer`}
+            >
+              <img
+                decoding="async"
+                loading="lazy"
+                src={BASE_MEDIA_URL + item.icon}
+                className="w-8 aspect-square h-8 "
+              />
+              <div className="flex flex-col justify-start items-start">
+                <div className="text-base text-N500 font-semibold">
+                  {item.name}
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No challenges available</div>
+        )}
+      </div>
+    </section>
+  );
 }
 
 export default TrekOverviewSection;
