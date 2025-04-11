382027f60f522de0d7dc47abef7e3355405f03af
phuyalbish <phuyalbish@gmail.com>
2025-04-11 22:55:24 +0545

    FAQ section

--- src/components/pages/indivisualtrek/TrekFAQSection.jsx
+++ src/components/pages/indivisualtrek/TrekFAQSection.jsx
@@ -7,7 +7,7 @@ function TrekFAQSection({ faqs }) {
       <div className="flex flex-col gap-2">
         {Array.isArray(faqs) && faqs.length > 0 ? (
           <>
-            <div className="text-xl font-bold">Frequently Asked Questions</div>
+            <div className="text-lg font-bold">Frequently Asked Questions</div>
             {faqs.map((faq, index) => (
               <TrekFAQ faq={faq} key={index} />
             ))}
