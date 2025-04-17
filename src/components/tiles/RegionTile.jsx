2f5eba2afef98e8a04bd33de078f7ac8f4df4f53
phuyalbish <phuyalbish@gmail.com>
2025-04-17 13:15:28 +0545

    Updated

--- src/components/tiles/RegionTile.jsx
+++ src/components/tiles/RegionTile.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-function RegionTile({ img, name, id }) {
+function RegionTile({ img, name, slug }) {
   return (
-    <Link to={`/r/${id}`}  className="p-2 w-full md:max-w-[400px]  cursor-pointer shadow-lg md:shadow-none m-2 hover:shadow-lg transition-all duration-300 ease-in-out rounded-md flex flex-col gap-3 bg-white">
+    <Link to={`/r/${slug}`}  className="p-2 w-full md:max-w-[400px]  cursor-pointer shadow-lg md:shadow-none m-2 hover:shadow-lg transition-all duration-300 ease-in-out rounded-md flex flex-col gap-3 bg-white">
       <p className="text-md font-light">{name}</p>
       <img
         decoding="async"
