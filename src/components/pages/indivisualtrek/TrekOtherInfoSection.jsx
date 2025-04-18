3a421070924b836965d3b35a3b2de3f30048aa87
phuyalbish <phuyalbish@gmail.com>
2025-04-18 20:17:26 +0545

    Update

--- src/components/pages/indivisualtrek/TrekOtherInfoSection.jsx
+++ src/components/pages/indivisualtrek/TrekOtherInfoSection.jsx
@@ -4,10 +4,10 @@ export const BASE_MEDIA_URL = import.meta.env.VITE_BASE_MEDIA_URL;
 function TrekOtherInfoSection({ info }) {
   const sanitizedContent = DOMPurify.sanitize(info);
   return (
-    <section id="overview" className="flex flex-col gap-5 text-base italic text-lighter">
+    <section id="overview" className="flex flex-col gap-5 text-base italic font-lighter">
 
-      Other Information:
-      <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
+      <span className="font-medium">Other Information:</span>
+      <span dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
     
     </section>
   );
