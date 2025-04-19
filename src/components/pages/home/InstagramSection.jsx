eb0a9bad3ba299c207c9d9f00518728faf0f2f9e
phuyalbish <phuyalbish@gmail.com>
2025-04-19 23:19:13 +0545

    Last Update

--- src/components/pages/home/InstagramSection.jsx
+++ src/components/pages/home/InstagramSection.jsx
@@ -34,14 +34,14 @@ function InstagramSection() {
         loading="lazy"
         src={right}
         alt=""
-        className="hidden md:block right-0 w-[9rem] object-cover absolute z-0"
+        className="hidden md:block right-0 top-48 w-[9rem] object-cover absolute z-0"
       />
       <img
         decoding="async"
         loading="lazy"
         src={left}
         alt=""
-        className="hidden md:block left-0 w-[9rem] object-cover absolute z-0"
+        className="hidden md:block left-0  top-48 w-[9rem] object-cover absolute z-0"
       />
       <div className="flex  w-full md:w-[50vw] container  gap-2 flex-col">
         <div className="flex flex-wrap justify-between items-center md:p-0 px-2">
