b805f4e45cc604c1704d8584b429539abd5405fb
phuyalbish <phuyalbish@gmail.com>
2025-04-20 12:26:38 +0545

    It is working

--- src/components/pages/indivisualtrek/TrekBasicInformationSection.jsx
+++ src/components/pages/indivisualtrek/TrekBasicInformationSection.jsx
@@ -121,7 +121,7 @@ function TrekBasicInformationSection({
  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-4 w-full rounded-md bg-G200">
         {trekBasicHeadings.map((item, index) =>
           item?.description ? (
-            <div className="rounded-xl flex gap-3  items-center justify-start">
+            <div key={index} className="rounded-xl flex gap-3  items-center justify-start">
 
                   {item?.icon &&
                       React.createElement(item?.icon, {
--- src/components/pages/indivisualtrek/TrekItenarySection.jsx
+++ src/components/pages/indivisualtrek/TrekItenarySection.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TrekItenaryAccordion from "@/components/accordion/TrekItenaryAccordion";
 
 function TrekItenarySection({ data }) {
+
+
+
+    const [scheduleData, setSchedule] = useState([]);
+  
+    const handleScheduleState = (id) => {
+     
+      setSchedule(
+        scheduleData.map((schedule) => {
+          if (id === schedule.id) {
+            schedule.isOpened = true;
+          } else {
+            schedule.isOpened = false;
+          }
+          return schedule;
+        })
+      );
+    };
+  
+    useEffect(() => {
+      if (Array.isArray(data) && data.length > 0) {
+        const updatedData = data.map((schedule, index) => ({
+          ...schedule,
+          isOpened: index === 0, 
+        }));
+        setSchedule(updatedData);
+  
+      }
+    }, [data]);
   return (
     <section id="itenary" className="flex flex-col gap-5">
       {Array.isArray(data) && data.length > 0 ? (
         <>
           <div className="text-xl font-liches font-light">Itinerary</div>
-          {data.map((item, index) => (
-            <TrekItenaryAccordion schedule={item} key={index} />
+          {scheduleData.map((item, index) => (
+            <TrekItenaryAccordion schedule={item} 
+              key={index} 
+              isOpened={item.isOpened}
+              handleScheduleState={handleScheduleState} />
           ))}
         </>
       ) : null}
