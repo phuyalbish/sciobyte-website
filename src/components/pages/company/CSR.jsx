b5e26b6d3670a197dcaaf8b136307c63783d5479
phuyalbish <phuyalbish@gmail.com>
2025-04-22 21:14:07 +0545

    Nepal Will removed from CSR

--- src/components/pages/company/CSR.jsx
+++ src/components/pages/company/CSR.jsx
@@ -20,53 +20,7 @@ const HomeStay = () => {
                   child irrespective of their socio-economic backgrounds should have a
                   fair shot at the stars.
                 </p>
-        <div className="flex flex-col lg:flex-row justify-between  items-center w-full gap-4 ">
-          
-          <div className="relative md:w-[32rem] w-full">
-            {/* <div className="absolute h-full w-full bg-gradient-to-t from-black via-transparent to-transparent"></div> */}
-            <img
-              decoding="async"
-              loading="lazy"
-              src={nepalWill}
-              alt="nepal will"
-              className="h-full w-full object-cover rounded-xl"
-            />
-            
-          </div> 
-          <div className="w-full lg:w-7/12 flex flex-col justify-center  ">
-            <h1 className="text-xl font-semibold text-left">
-              Nepal <span className="text-G300">Will</span>
-            </h1>
-            <a
-              href="https://nepalwill.com/"
-              target="_future"
-              className=" bottom-1 right-1 flex gap-2 mb-3  items-center "
-            >
-              <img
-                decoding="async"
-                loading="lazy"
-                src={NW}
-                alt="Nepal Will"
-                className="w-6 rounded-full"
-              />
-              <p className="text-sm font-light  text-black">
-                @ <span className="underline underline-offset-2">nepalwill</span> 
-              </p>
-            </a>
-            <p className="text-justify">
-              NepalWill is a non-governmental organization dedicated to
-              uplifting primary school children in Nepal by providing essential
-              educational resources and opportunities. Believing in the
-              transformative power of education, NepalWill works to bridge the
-              resource gap that hinders many children from accessing quality
-              learning environments. What sets NepalWill apart is its commitment
-              to transparency and integrity by exclusively accepting in-kind
-              contributions such as books, school supplies, and infrastructure
-              support. This ensures that every donation makes a tangible
-              difference in the lives of the children we aim to serve.
-            </p>
-          </div>
-        </div>
+        
       </div>
     </>
   );
