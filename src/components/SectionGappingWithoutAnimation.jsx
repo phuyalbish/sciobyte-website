b432f815307eb6415f0444e7cbc261ccb4e79e76
phuyalbish <phuyalbish@gmail.com>
2025-05-01 20:50:11 +0545

    Up

--- src/components/SectionGappingWithoutAnimation.jsx
+++ src/components/SectionGappingWithoutAnimation.jsx
@@ -1,7 +1,7 @@
 function SectionGappingWithoutAnimation({children}) {
     return (
 
-    <div className="hidden md:grid grid-cols-1 w-full md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 w-full md:grid-cols-2 lg:grid-cols-3 gap-4">
         {children}</div>
   )
 }
