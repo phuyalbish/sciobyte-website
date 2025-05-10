604164fd006d263d99ed41c2a4ad644d593c2974
phuyalbish <phuyalbish@gmail.com>
2025-05-10 20:15:17 +0545

    Lottee added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import AppRoutes from "@/router/router.jsx";
 import ScrollToTop from "@/router/ScrollToTop.jsx";
 import "./App.css";
 
+import Animation from "@/data/Animation.json"
+import Lottie from "lottie-react";
+
 function App() {
   return (
     <Router>
@@ -11,7 +14,10 @@ function App() {
       {/* <PageLayout>
         <AppRoutes />
       </PageLayout> */}
-      <div className="w-full h-[100vh] flex items-center justify-center">
+      <div className="w-full h-[100vh] flex  flex-col items-center justify-center">
+        <div className=" w-[90vw] md:w-[50vw] ">
+        <Lottie animationData={Animation}  loop={true}/>
+        </div>
             <div className="text-xl font-liches">Under Construction !</div>
       </div>
     </Router>
