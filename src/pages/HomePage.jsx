db3a001a7f8480fa96d26d2cad36696a78c4f6a4
phuyalbish <phuyalbish@gmail.com>
2025-06-23 11:47:20 +0545

    Updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
+import Logo from "@/assets/Logo.png"
 
 function HomePage() {
 
@@ -11,7 +12,8 @@ function HomePage() {
   return (
       <div className="realtive text-white h-full w-full flex flex-col items-center justify-center">
           <HoverableGrid/>
-          <div className="flex flex-col gap-4  fixed top-1/2 -translate-y-1/2">
+          <div className="flex flex-col gap-4 items-center  justify-center fixed top-1/2 -translate-y-1/2">
+          <img src={Logo} alt="Webodle Logo"  width={100} height={100}/>
                 <div className="text-white text-lg select-none hover:text-blue-500 transition-colors duration-500 ease-in-out cursor-pointer">Webodle is under construction!</div>
                 <div className="flex gap-4 justify-center w-full">
                       <a href="http://instagram.com/webodle" rel="noopener noreferrer"  target="_blank"><FaInstagram className="size-8 select-none hover:text-blue-500 transition-colors duration-500 ease-in-out cursor-pointer"/></a>
