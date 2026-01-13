b102a882a77ca11a1e0dd8dfbb012d9cbb389d88
phuyalbish <phuyalbish@gmail.com>
2026-01-13 12:38:36 +0545

    [fix] cloudflare done

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,15 +1,27 @@
 import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
 addEventListener('fetch', event => {
-  event.respondWith(handleEvent(event));
+  event.respondWith(handleRequest(event));
 });
 
-async function handleEvent(event) {
+async function handleRequest(event) {
   try {
+    // Attempt to serve the asset as requested (CSS, JS, Images, etc.)
     return await getAssetFromKV(event);
-  } catch {
-    return await getAssetFromKV(event, {
-      mapRequestToAsset: req => new Request('/index.html', req)
-    });
+  } catch (e) {
+    // If the asset isn't found (likely a frontend route like /dashboard),
+    // we force the request to look for index.html
+    const url = new URL(event.request.url);
+    url.pathname = '/index.html'; 
+    
+    const spaRequest = new Request(url.toString(), event.request);
+    
+    try {
+      return await getAssetFromKV(event, {
+        mapRequestToAsset: () => spaRequest,
+      });
+    } catch (innerError) {
+      return new Response("Resource not found", { status: 404 });
+    }
   }
 }
